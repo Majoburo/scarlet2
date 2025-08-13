cb75c81165352ca1e3c5464bbadd241d81d75ecd
majoburo <majoburo@gmail.com>
2025-08-13 17:07:19 -0500

    more tests

--- tests/scarlet2/plot_mcmc.py
+++ tests/scarlet2/plot_mcmc.py
@@ -1,8 +1,13 @@
 from scarlet2.io import load_session_h5
 from scarlet2 import plot
+from matplotlib import pyplot as plt
+import sys
 
-scene2, obs2, samples, meta = load_session_h5("obj.h5", id=0, path="runs")
+scene2, obs2, samples, meta = load_session_h5(sys.argv[1], id=0, path="runs")
 
-plot.mcmc_scene(obs2, scene2, samples)
-plot.mcmc_diagnostics(obs2, scene2, samples, centers=meta["centers"],recenter=False)
-plot.corner_centers_spectra(samples, scene2)
+fig,ax = plot.mcmc_scene(obs2, scene2, samples)
+plt.show()
+fig,ax =plot.mcmc_diagnostics(obs2, scene2, samples, centers=meta["centers"], recenter=False)
+plt.show()
+fig,ax =plot.corner_centers_spectra(samples, scene2)
+plt.show()
